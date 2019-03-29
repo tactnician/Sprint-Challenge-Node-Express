0abcd1e0a9de5f080d88955ba8ee10aafeb42afe
tactnician <tactnician@gmail.com>
2019-03-29 13:41:18 -0400

    finished crud operation on both end points

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -2,22 +2,69 @@ const express = require('express');
 
 const router = express.Router();
 
-// const Actions= require('../data/helpers/actionModel');
+const Actions= require('../data/helpers/actionModel');
 
-// //get()
-// router.get();
+//get()
+router.get('/', async (req, res) => {
+    try {
+        const id =  await req.params.id;
+        const resourceArr = await Actions.get(id);
+        console.log(" resource Array :" , resourceArr);
+        res.status(200).json(resourceArr);
+    } catch (error) {
+        console.log(`error in get method ${error}`);
+        res.status(500).json({message: `error getting resources: ${error}`});
+    }
+});
 
-// //getById()
-// router.get();
+//getById()
+router.get('/:id', async (req, res) => {
+    try {
+        // const {id} =  await req.params.id;
+        const resourceArr = await Actions.get(req.params.id);
+        console.log(" resource Array :" , resourceArr);
+        res.status(200).json(resourceArr);
+    } catch (error) {
+        console.log(`error in get method ${error}`);
+        res.status(500).json({message: `error getting resources: ${error}`});
+    }
+});
 
-// //insert()
-// router.post();
+//insert()
+router.post('/', async (req, res) => {
+    try {
+        const newResource = await Actions.insert(req.body)
+        res.status(200).json(newResource);
+    } catch (error) {
+        res.status(500).json(`error creating resource ${error}`)
+    }
+});
 
-// //update()
-// router.put();
 
-// //remove()
-// router.delete();
+//update()
+router.put('/:id', async (req, res) => {
+    try {
+        const resourceId = await Actions.get(req.params.id);
+        if (resourceId) {
+            const updated = await Projects.update(req.params.id, req.body);
+            res.status(200).json(updated);
+        } else {
+            res.status(404).json({message: `resource not found`});
+        }
+    } catch (error) {
+        res.status(500).json(`error updating resource ${error}`);
+    }
+});
+
+//remove()
+router.delete('/:id', async (req, res) => {
+    try {
+        const quantityDeleted = await Actions.remove(req.params.id);
+        res.status(200).json(quantityDeleted);
+    } catch (error) {
+        res.status(500).json(`error removing resource: ${error}`);
+    }
+});
 
 
 module.exports = router;
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -18,9 +18,7 @@ router.get('/', async (req, res) => {
 });
 
 
-// //getById()
-// router.get();
-
+//getById()
 router.get('/:id', async (req, res) => {
     try {
         // const {id} =  await req.params.id;
@@ -34,14 +32,40 @@ router.get('/:id', async (req, res) => {
 });
 
 
-// //insert()
-// router.post();
+//insert()
+router.post('/', async (req, res) => {
+    try {
+        const newResource = await Projects.insert(req.body)
+        res.status(200).json(newResource);
+    } catch (error) {
+        res.status(500).json(`error creating resource ${error}`)
+    }
+});
 
-// //update()
-// router.put();
+//update()
+router.put('/:id', async (req, res) => {
+    try {
+        const resourceId = await Projects.get(req.params.id);
+        if (resourceId) {
+            const updated = await Projects.update(req.params.id, req.body);
+            res.status(200).json(updated);
+        } else {
+            res.status(404).json({message: `resource not found`});
+        }
+    } catch (error) {
+        res.status(500).json(`error updating resource ${error}`);
+    }
+});
 
-// //remove()
-// router.delete();
+//remove()
+router.delete('/:id', async (req, res) => {
+    try {
+        const quantityDeleted = await Projects.remove(req.params.id);
+        res.status(200).json(quantityDeleted);
+    } catch (error) {
+        res.status(500).json(`error removing resource: ${error}`);
+    }
+});
 
 
 module.exports = router;
